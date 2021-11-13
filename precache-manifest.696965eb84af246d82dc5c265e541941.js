7146fbebb050eeb3a38518896f16698cc2413e7c
Sidneyp91 <92775884+Sidneyp91@users.noreply.github.com>
2021-11-13 09:16:42 +0000

    Deploying to gh-pages from  @ 312746f4b400de281292b202b8159127e82996f4 🚀

---
+++ precache-manifest.696965eb84af246d82dc5c265e541941.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3593b9418462423704f730f2f5e59c6",
+    "url": "/pjnr/index.html"
+  },
+  {
+    "revision": "f6f71771bcc8f7d94eea",
+    "url": "/pjnr/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "03bba90ddfcb5aba7167",
+    "url": "/pjnr/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "cd8fbd0200726383e8f1",
+    "url": "/pjnr/static/css/main.e0688521.chunk.css"
+  },
+  {
+    "revision": "2191b501f2a8c66d5842",
+    "url": "/pjnr/static/js/2.3dca1cbb.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/pjnr/static/js/2.3dca1cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f71771bcc8f7d94eea",
+    "url": "/pjnr/static/js/3.142505ee.chunk.js"
+  },
+  {
+    "revision": "03bba90ddfcb5aba7167",
+    "url": "/pjnr/static/js/4.6d9e7a09.chunk.js"
+  },
+  {
+    "revision": "cd8fbd0200726383e8f1",
+    "url": "/pjnr/static/js/main.0c14b7f2.chunk.js"
+  },
+  {
+    "revision": "41d1a2c16fdc6c289c98",
+    "url": "/pjnr/static/js/runtime-main.48ddd4d5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/pjnr/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/pjnr/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/pjnr/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/pjnr/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "08305dd6658adb05a81d5bc522f30621",
+    "url": "/pjnr/static/media/dekutLogo.08305dd6.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/pjnr/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/pjnr/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "1bdaad98ae5d4ede7c0d4a1206978229",
+    "url": "/pjnr/static/media/javaOrientLogo.1bdaad98.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/pjnr/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "9b2a1852b31dd70426d1c6844cab8d77",
+    "url": "/pjnr/static/media/morLogo.9b2a1852.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/pjnr/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "45a28e0988fbc7e462fe0b15dde35149",
+    "url": "/pjnr/static/media/nyeriLogo.45a28e09.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/pjnr/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "ce23a83db783b948f54ee4528a2f7610",
+    "url": "/pjnr/static/media/rivercrossLogo.ce23a83d.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/pjnr/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/pjnr/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "160e868b502eec6117b0361ea1e19840",
+    "url": "/pjnr/static/media/syntaxLogo.160e868b.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/pjnr/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "0541612c849c233c46ad5f431b1a7c22",
+    "url": "/pjnr/static/media/theatreLogo.0541612c.png"
+  }
+]);
